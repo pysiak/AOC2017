f5c8d52be57a279805b99d14bdf39d5568e4e3b8
mks48641 <mks48641@gsk.com>
2017-12-01 12:31:40 +0100

    Day 01: second star

---
+++ advent1b/advent1b.go
@@ -0,0 +1,51 @@
+// build with go build advent1b.go
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"strconv"
+	"os"
+)
+
+// [A] sum of all digits that match the next digit in the list.
+// [B] The list is circular, so the digit after the last digit is the first digit in the list.
+func processLine(line string) {
+	var code int
+	length := len(line)
+	halfWay := length / 2
+
+	for i := 0 ; i < length ; i++ {
+		nextPos := i + halfWay
+
+		// [B] next digit for last is the first in the list
+		if (nextPos == length) {
+			nextPos = 0
+		}
+		
+		// skipped error checking
+		current, _ := strconv.Atoi(string(line[i]))
+		next, _ := strconv.Atoi(string(line[nextPos % length]))
+
+		if current == next {
+			// [A] sum
+			code = code + current
+		}
+	}
+
+	fmt.Println(code)
+}
+
+
+func main() {
+
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+
+	fscanner := bufio.NewScanner(file)
+
+	for fscanner.Scan() {
+		processLine(fscanner.Text())
+	}
+
+}
